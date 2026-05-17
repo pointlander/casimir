d4043959e6d6854b9284d8f91a2359398fe822c6
Andrew Snodgrass <pointlander@gmail.com>
2026-05-16 19:34:06 -0500

    Fixed bug

--- main.go
+++ main.go
@@ -163,6 +163,7 @@ func (n *Neuron) Iterate(iterations int) {
 		x := n.Set.ByName["x"]
 		y := n.Set.ByName["y"]
 		for i, value := range x.X {
+			x.X[i] = (value + y.X[i]) / 2
 			y.X[i] = (value + y.X[i]) / 2
 		}
 	}
